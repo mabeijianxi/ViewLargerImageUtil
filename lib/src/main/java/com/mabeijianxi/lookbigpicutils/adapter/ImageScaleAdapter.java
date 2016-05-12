5f47c071d7efb91b99fa6e844c6855611dc81428
mabeijianxi <mabeijianxi@gmail.com>
2016-05-11 22:11:03 -0700

    保存图片到本地

--- lib/src/main/java/com/mabeijianxi/lookbigpicutils/adapter/ImageScaleAdapter.java
+++ lib/src/main/java/com/mabeijianxi/lookbigpicutils/adapter/ImageScaleAdapter.java
@@ -1,5 +1,6 @@
 package com.mabeijianxi.lookbigpicutils.adapter;
 
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.support.v4.view.PagerAdapter;
@@ -13,7 +14,9 @@ import com.mabeijianxi.lookbigpicutils.activty.LookBigPicActivity;
 import com.mabeijianxi.lookbigpicutils.bean.PicDataBean;
 import com.mabeijianxi.lookbigpicutils.photoview.PhotoView;
 import com.mabeijianxi.lookbigpicutils.photoview.PhotoViewAttacher;
+import com.mabeijianxi.lookbigpicutils.utils.CommonUtils;
 import com.mabeijianxi.lookbigpicutils.utils.ImageUtils;
+import com.mabeijianxi.lookbigpicutils.view.SendDongTaiDialog;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.assist.FailReason;
@@ -27,25 +30,31 @@ import java.util.List;
  * Created by mabeijianxi on 2015/1/5.
  */
 public class ImageScaleAdapter extends PagerAdapter implements PhotoViewAttacher.OnPhotoTapListener {
-    private List<PicDataBean> mPicData;
-    private Context mContext;
     private ImageLoader mImageLoader = ImageLoader.getInstance();
+    private SendDongTaiDialog sendDongTaiDialog;
+    private List<PicDataBean> mPicData;
+    private PhotoView imageView;
+    private View mCurrentView;
+    private Context mContext;
     private DisplayImageOptions pager_options = new DisplayImageOptions.Builder()
             .showImageForEmptyUri(R.drawable.home_youpin)
             .showImageOnFail(R.drawable.home_youpin)
-            .cacheOnDisk(true)
-            .cacheInMemory(true)
             .imageScaleType(ImageScaleType.EXACTLY)//会对图片进一步的缩放
             .bitmapConfig(Bitmap.Config.RGB_565)//此种模消耗的内存会很小,2个byte存储一个像素
             .considerExifParams(true)
+            .cacheInMemory(true)
+            .cacheOnDisk(true)
             .build();
-    private PhotoView imageView;
-    private View mCurrentView;
 
     public ImageScaleAdapter(Context mContext, List<PicDataBean> data) {
         super();
         this.mPicData = data;
         this.mContext = mContext;
+        if (sendDongTaiDialog == null) {
+            sendDongTaiDialog = new SendDongTaiDialog((Activity) mContext);
+            sendDongTaiDialog.getBtn1().setText("本地保存");
+            sendDongTaiDialog.getBtn2().setVisibility(View.GONE);
+        }
     }
 
     @Override
@@ -78,10 +87,11 @@ public class ImageScaleAdapter extends PagerAdapter implements PhotoViewAttacher
         imageView = (PhotoView) inflate.findViewById(R.id.pv);
         imageView.setOnPhotoTapListener(this);
         final ProgressBar pb = (ProgressBar) inflate.findViewById(R.id.pb);
-        final PicDataBean ealuationPicBean = mPicData.get(position);
+        final PicDataBean mPicDataBean = mPicData.get(position);
 
-        if (ealuationPicBean != null && ealuationPicBean.imageBigUrl != null && !"null".equals(ealuationPicBean.imageBigUrl)) {
-            setupNetImage(pb, ealuationPicBean);
+        if (mPicDataBean != null && mPicDataBean.imageBigUrl != null && !"null".equals(mPicDataBean.imageBigUrl)) {
+            setupNetImage(pb, mPicDataBean);
+            savaPic(mPicDataBean);
         } else {
             imageView.setImageResource(R.drawable.home_youpin);
         }
@@ -90,8 +100,34 @@ public class ImageScaleAdapter extends PagerAdapter implements PhotoViewAttacher
         return inflate;
     }
 
+    private void savaPic(final PicDataBean ealuationPicBean) {
+
+        imageView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                sendDongTaiDialog.setmDialogClickListener(new SendDongTaiDialog.DialogClickListener() {
+                    @Override
+                    public void onClick(int id) {
+                        if (id == R.id.btn_take_photo) {
+                            Bitmap bitmap = ImageUtils.getBitmapFromCache(ealuationPicBean.imageBigUrl, mImageLoader);
+                            if (bitmap != null) {
+                                CommonUtils.saveImageToGallery(mContext, bitmap);
+                                CommonUtils.soonToast(mContext, "保存成功");
+                            } else {
+                                CommonUtils.soonToast(mContext, "保存失败");
+                            }
+                        }
+                    }
+                });
+                sendDongTaiDialog.show();
+                return true;
+            }
+        });
+    }
+
     /**
      * 设置网络图片加载规则
+     *
      * @param pb
      * @param ealuationPicBean
      */
@@ -105,17 +141,17 @@ public class ImageScaleAdapter extends PagerAdapter implements PhotoViewAttacher
 
             @Override
             public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
-                overLoad( pb);
+                overLoad(pb);
             }
 
             @Override
             public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
-                overLoad( pb);
+                overLoad(pb);
             }
 
             @Override
             public void onLoadingCancelled(String imageUri, View view) {
-                overLoad( pb);
+                overLoad(pb);
             }
         });
     }
@@ -140,7 +176,7 @@ public class ImageScaleAdapter extends PagerAdapter implements PhotoViewAttacher
      *
      * @param pb
      */
-    private void startLoad( ProgressBar pb) {
+    private void startLoad(ProgressBar pb) {
         pb.setVisibility(View.VISIBLE);
     }
 
@@ -149,7 +185,7 @@ public class ImageScaleAdapter extends PagerAdapter implements PhotoViewAttacher
      *
      * @param pb
      */
-    private void overLoad( ProgressBar pb) {
+    private void overLoad(ProgressBar pb) {
         pb.setVisibility(View.GONE);
     }
 
--- lib/src/main/java/com/mabeijianxi/lookbigpicutils/utils/CommonUtils.java
+++ lib/src/main/java/com/mabeijianxi/lookbigpicutils/utils/CommonUtils.java
@@ -2,20 +2,31 @@ package com.mabeijianxi.lookbigpicutils.utils;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.Rect;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 /**
  * Created by mabeijianxi on 2016/1/13.
  */
 public class CommonUtils {
-//    public static BuildConfig.LOG_DEBUG=false;
+    private static Toast toast;
+    //    public static BuildConfig.LOG_DEBUG=false;
 
     public static int getScreenSizeWidth(Activity con) {
         DisplayMetrics metric = new DisplayMetrics();
@@ -129,5 +140,42 @@ public class CommonUtils {
         Rect frame = new Rect();
         v.getWindowVisibleDisplayFrame(frame);
         return frame.top;
+    }
+    public static void soonToast(Context context, String text) {
+        if (toast == null) {
+            toast = Toast.makeText(context, text, Toast.LENGTH_SHORT);
+        }
+        toast.setText(text);//将文本设置为toast
+        toast.show();
+
+    }
+    public static void saveImageToGallery(Context context, Bitmap bmp) {
+        // 首先保存图片
+        File appDir = new File(Environment.getExternalStorageDirectory(), "DownloadJianXi");
+        if (!appDir.exists()) {
+            appDir.mkdir();
+        }
+        String fileName = System.currentTimeMillis() + ".jpg";
+        File file = new File(appDir, fileName);
+        try {
+            FileOutputStream fos = new FileOutputStream(file);
+            bmp.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+            fos.flush();
+            fos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // 其次把文件插入到系统图库
+        try {
+            MediaStore.Images.Media.insertImage(context.getContentResolver(),
+                    file.getAbsolutePath(), fileName, null);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        // 最后通知图库更新
+        context.sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.parse("file://" + appDir.getPath())));
     }
 }
